f9c767eee4577c7f03fe2a61c21b3b828bf80c46
timmy251204 <timparshin@icloud.com>
2021-04-03 00:52:56 +0300

    Decorator

---
+++ API/decorator.py
@@ -0,0 +1,70 @@
+user = {
+    'name': 'masha',
+    'access_level': 'admin'
+}
+Password = 'Brawl_stars'
+def benchmark(func):
+    import time
+
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print('[*] Время выполнения: {} секунд.'.format(end - start))
+
+    return wrapper
+
+
+def secure_function(func):
+    def wrapper(*args, **kwargs):
+        if user['access_level'] == 'admin':
+            return func(*args, **kwargs)
+        return 'Access denied'
+
+    return wrapper
+
+
+def check_password(func):
+    checked = 0
+    def wrapper(*args, **kwargs):
+        nonlocal checked
+        if checked <= 0:
+            print('ВВЕДИТЕ ПАРОЛЬ')
+            check = input()
+            if check == Password:
+                checked += 1
+                return func(*args, **kwargs)
+            else:
+                print('В доступе отказано')
+        else:
+            return func(*args, **kwargs)
+
+
+    return wrapper
+
+
+
+
+
+
+
+def get_secure_information():
+    return 'My super password is qwerty123'
+# print_ = print
+# print = lambda *args: print_(' '.join(map(str.upper, args))) Print Capsom
+
+
+
+@check_password
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+
+
+
+
+
+print(fibonacci(8))
